9d2671c2f1d7195170a9194d02158c6e7b2839cb
Rajeswaran <rajeswaran149@gmail.com>
2024-09-24 11:02:00 +0530

    update vercel deploy url

--- src/app/config.js
+++ src/app/config.js
@@ -1,2 +1,2 @@
-export const  base_url ="http://localhost:5000"
-// export const  base_url ="https://system-controller-server.vercel.app"
+// export const  base_url ="http://localhost:5000"
+export const  base_url ="https://todo-server-gilt-nine.vercel.app"
